c97a30e9290c06310f0f26b5a0f0346b905c7ae1
autumn-ragland <raglanda998@gmail.com>
2020-09-23 13:21:22 -0500

    style form and update display name

--- ClothingBoutique/Models/ItemModel.cs
+++ ClothingBoutique/Models/ItemModel.cs
@@ -6,12 +6,20 @@ namespace ClothingBoutique.Models
     {
         [Key]
         public int id{get;set;}
+        [Display(Name="Name")]
         public string itemName{get;set;}
-        // TODO : Add error message
-        [Range(0,200)]
+
+        [Display(Name="Price")]
+        [Range(0,200, ErrorMessage = "An item cannot be more than $200")]
         public int price{get;set;}
+
+        [Display(Name="Category")]
         public string itemCategory{get;set;}
+
+        [Display(Name="In Stock")]
         public bool inStock{get;set;}
+
+        [Display(Name="Featured")]
         public bool featured{get;set;}
     }
 }
